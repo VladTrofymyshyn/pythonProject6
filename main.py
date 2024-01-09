69d39563ec8ba467d049c9f5b19e1d23647f0d00
VladTrofymyshyn <vladtrofymyshyn@gmail.com>
2024-01-09 17:07:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("debug")
+logging.info("info abt code")
+logging.warning("warning abt posibility of error")
+logging.error("error in program")
+logging.critical("critical error")
